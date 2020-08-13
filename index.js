40ffd648aa5f7fb4d50502be918cf2eac9b586ca
HitsujiRere <hitsujirere@gmail.com>
2020-08-13 23:53:09 +0900

    Add index.js and npm install express and body-parser.

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', async (req, res) => {
+    res.status(200).end();
+});
+
+app.listen(PORT, async (req, res) => {
+    console.log('Server is up!');
+});
