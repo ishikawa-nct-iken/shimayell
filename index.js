00128ee61e9ccd79536ad998d65fd662ce8dd9d1
HitsujiRere <hitsujirere@gmail.com>
2020-08-14 00:10:54 +0900

    Add 404 and 500 page.

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ app.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+app.use(async (req, res, next) => {
+    res.status(404);
+    res.render('err404.ejs');
+});
+
+app.use(async (err, req, res, next) => {
+    res.status(500);
+    res.render('err500.ejs');
+    console.log(err);
+});
+
 app.listen(PORT, async (req, res) => {
     console.log('Server is up!');
 });
